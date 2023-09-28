01d0b2e7e01fa025f81c26ae5b192d9b2b67a4e7
iamjoaovictor23 <viictorcabraal@outlook.com>
2023-09-28 20:22:08 -0300

    Construção de buttões em tela mobile

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <ComponenteTest nome ='Victor'/>
+      <ComponenteTest nome ='Victor '/>
       <StatusBar style="auto" />
     </View>
   );
--- componente.js
+++ componente.js
@@ -1,14 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export function ComponenteTest({nome}){
         
-        const [data,setData]=useState('Joao Victor');
+        const [data,setData]=useState(2);
+
+        useEffect(()=>{
+            alert('DA NÃO IDIOTA')
+        },[data])
     
         return(
             <View>
             <Text>{nome}{data}</Text>
-            <Button onPress={()=>setData(9)} title='clique aqui'/>
+            <Button onPress={()=>setData(data+1)} title='Contador'/>
+            <Button onPress={()=>setData(data-1)} title='Menos'/>
+            <Button onPress={()=>setData(0)} title='Reset contador'/>
+            <Button onPress={()=>setData(data+1)} title='clique aqui'/>
             </View>
         );
 }
